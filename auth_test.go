36be71970dede62729c4dd19c50d0488f6d4d738
Gabor Retvari <retvari@tmit.bme.hu>
2023-03-28 21:33:17 +0200

    test: add watcher tests

--- auth_test.go
+++ auth_test.go
@@ -108,7 +108,7 @@ func TestPlaintextAuth(t *testing.T) {
 	defer report()
 
 	loggerFactory := logger.NewLoggerFactory(authTestLoglevel)
-	log := loggerFactory.NewLogger("test")
+	log := loggerFactory.NewLogger("test-plaintext")
 
 	log.Debug("writing config file")
 	f, err := os.CreateTemp("", "stunner_conf_*.yaml")
@@ -419,7 +419,7 @@ func TestLongtermAuth(t *testing.T) {
 	defer report()
 
 	loggerFactory := logger.NewLoggerFactory(authTestLoglevel)
-	log := loggerFactory.NewLogger("test")
+	log := loggerFactory.NewLogger("test-longterm")
 
 	log.Debug("writing config file")
 	f, err := os.CreateTemp("", "stunner_conf_*.yaml")
@@ -678,4 +678,322 @@ func TestLongtermAuth(t *testing.T) {
 	_, ok = authHandler(*iceAuth.Username, v1alpha1.DefaultRealm,
 		&net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 1234})
 	assert.False(t, ok, "authHandler key ok")
+}
+
+func TestWatcher(t *testing.T) {
+	lim := test.TimeOut(time.Second * 120)
+	defer lim.Stop()
+
+	report := test.CheckRoutines(t)
+	defer report()
+
+	loggerFactory := logger.NewLoggerFactory(authTestLoglevel)
+	log := loggerFactory.NewLogger("test-watch")
+
+	log.Debug("editing config file")
+	watchTestConfig := v1alpha1.StunnerConfig{}
+	basicAuthTestConfig.DeepCopyInto(&watchTestConfig)
+
+	// remove the last listener
+	watchTestConfig.Listeners =
+		watchTestConfig.Listeners[:len(watchTestConfig.Listeners)-1]
+
+	log.Debug("writing config file")
+	f, err := os.CreateTemp("", "stunner_conf_*.yaml")
+	assert.NoError(t, err, "creating temp config file")
+	defer os.Remove(f.Name())
+
+	data, err := json.MarshalIndent(watchTestConfig, "", "  ")
+	assert.NoError(t, err, "JSONify config file")
+
+	_, err = f.Write(data)
+	assert.NoError(t, err, "writing JSON config file")
+
+	log.Debug("starting auth server")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// set watch=true
+	handler, err := handler.NewHandler(ctx, f.Name(), authTestLoglevel, true)
+	assert.NoError(t, err, "create handler")
+
+	router := server.HandlerWithOptions(handler, server.GorillaServerOptions{})
+	httpServer := &http.Server{Addr: ":8088", Handler: router}
+	defer httpServer.Close()
+
+	go func() { _ = httpServer.ListenAndServe() }()
+
+	log.Debug("starting auth client")
+	authClient, err := client.NewClient("http://:8088")
+	assert.NoError(t, err, "new client")
+
+	// starting a Stunner instance to use its authenticator
+	log.Debug("creating a stunnerd")
+	s := stunner.NewStunner(stunner.Options{
+		DryRun:   true,
+		LogLevel: authTestLoglevel,
+	})
+	defer s.Close()
+	assert.NoError(t, s.Reconcile(watchTestConfig), "reconcile")
+	authHandler := s.NewAuthHandler()
+
+	log.Debug("--------------------------------------")
+	log.Debug("testing initial TURN auth with username and service set")
+	u := "dummy"
+	param := client.GetTurnAuthParams{Service: types.GetTurnAuthParamsServiceTurn, Username: &u}
+	turnAuth, err := authClient.GetTurnAuthToken(ctx, &param)
+	assert.NoError(t, err, "TURN auth get")
+
+	assert.NotNil(t, turnAuth, "TURN token nil")
+	assert.NotNil(t, turnAuth.Username, "username nil")
+	// cannot override username for plaintext
+	assert.Equal(t, "user1", *turnAuth.Username, "username nil")
+	assert.NotNil(t, turnAuth.Password, "password nil")
+	assert.Equal(t, "pass1", *turnAuth.Password, "password ok")
+	assert.NotNil(t, turnAuth.Ttl, "ttl nil")
+	assert.Equal(t, int64(86400), *turnAuth.Ttl, "ttl ok")
+	assert.NotNil(t, turnAuth.Uris, "URIs nil")
+	uris := *turnAuth.Uris
+	assert.Contains(t, uris, "turn:1.2.3.4:3478?transport=udp", "UDP URI")
+	assert.Contains(t, uris, "turn:1.2.3.4:3478?transport=tcp", "TCP URI")
+	assert.Contains(t, uris, "turns:127.0.0.1:3479?transport=tcp", "TLS URI")
+
+	key, ok := authHandler(*turnAuth.Username, v1alpha1.DefaultRealm,
+		&net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 1234})
+	assert.True(t, ok, "authHandler key ok")
+	assert.Equal(t, key, a12n.GenerateAuthKey(*turnAuth.Username,
+		v1alpha1.DefaultRealm, *turnAuth.Password), "auth handler ok")
+
+	log.Debug("--------------------------------------")
+	log.Debug("editing config file: change username")
+	watchTestConfig.Auth.Credentials["username"] = "newuser"
+	data, err = json.MarshalIndent(watchTestConfig, "", "  ")
+	assert.NoError(t, err, "JSONify config file")
+
+	assert.NoError(t, f.Truncate(0), "truncate temp file")
+	_, err = f.Seek(0, 0)
+	assert.NoError(t, err, "seek temp file")
+	_, err = f.Write(data)
+	assert.NoError(t, err, "writing JSON config file")
+
+	// let STUNner pick up the config
+	assert.NoError(t, s.Reconcile(watchTestConfig), "reconcile")
+
+	// wait so that the auth-server has comfortable time to pick up the new config
+	time.Sleep(time.Duration(100) * time.Millisecond)
+
+	param = client.GetTurnAuthParams{Service: types.GetTurnAuthParamsServiceTurn, Username: &u}
+	turnAuth, err = authClient.GetTurnAuthToken(ctx, &param)
+	assert.NoError(t, err, "TURN auth get")
+
+	assert.NotNil(t, turnAuth, "TURN token nil")
+	assert.NotNil(t, turnAuth.Username, "username nil")
+	assert.Equal(t, "newuser", *turnAuth.Username, "username nil")
+	assert.NotNil(t, turnAuth.Password, "password nil")
+	assert.Equal(t, "pass1", *turnAuth.Password, "password ok")
+	assert.NotNil(t, turnAuth.Ttl, "ttl nil")
+	assert.Equal(t, int64(86400), *turnAuth.Ttl, "ttl ok")
+	assert.NotNil(t, turnAuth.Uris, "URIs nil")
+	uris = *turnAuth.Uris
+	assert.Contains(t, uris, "turn:1.2.3.4:3478?transport=udp", "UDP URI")
+	assert.Contains(t, uris, "turn:1.2.3.4:3478?transport=tcp", "TCP URI")
+	assert.Contains(t, uris, "turns:127.0.0.1:3479?transport=tcp", "TLS URI")
+
+	key, ok = authHandler(*turnAuth.Username, v1alpha1.DefaultRealm,
+		&net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 1234})
+	assert.True(t, ok, "authHandler key ok")
+	assert.Equal(t, key, a12n.GenerateAuthKey(*turnAuth.Username,
+		v1alpha1.DefaultRealm, *turnAuth.Password), "auth handler ok")
+
+	log.Debug("--------------------------------------")
+	log.Debug("editing config file: change auth type")
+	watchTestConfig.Auth = v1alpha1.AuthConfig{
+		Type: "longterm",
+		Credentials: map[string]string{
+			"secret": "my-secret",
+		},
+	}
+	data, err = json.MarshalIndent(watchTestConfig, "", "  ")
+	assert.NoError(t, err, "JSONify config file")
+
+	assert.NoError(t, f.Truncate(0), "truncate temp file")
+	_, err = f.Seek(0, 0)
+	assert.NoError(t, err, "seek temp file")
+	_, err = f.Write(data)
+	assert.NoError(t, err, "writing JSON config file")
+
+	// let STUNner pick up the config
+	assert.NoError(t, s.Reconcile(watchTestConfig), "reconcile")
+
+	// wait so that the auth-server has comfortable time to pick up the new config
+	time.Sleep(time.Duration(100) * time.Millisecond)
+
+	u = "dummy"
+	param = client.GetTurnAuthParams{Username: &u}
+	turnAuth, err = authClient.GetTurnAuthToken(ctx, &param)
+	assert.NoError(t, err, "TURN auth get")
+
+	assert.NotNil(t, turnAuth, "TURN token nil")
+	assert.NotNil(t, turnAuth.Username, "username nil")
+	// we do not know the username, only that it contains the timestamps plus a colon
+	assert.Regexp(t, regexp.MustCompile(`^\d+:dummy$`), *turnAuth.Username, "username ok")
+	assert.NoError(t, a12n.CheckTimeWindowedUsername(*turnAuth.Username), "username valid")
+	assert.NotNil(t, turnAuth.Password, "password nil")
+	passwd, err := a12n.GetLongTermCredential(*turnAuth.Username, "my-secret")
+	assert.NoError(t, err, "GetLongTermCredential")
+	assert.Equal(t, passwd, *turnAuth.Password, "password ok")
+	assert.NotNil(t, turnAuth.Uris, "URIs nil")
+	uris = *turnAuth.Uris
+	assert.Contains(t, uris, "turn:1.2.3.4:3478?transport=udp", "UDP URI")
+	assert.Contains(t, uris, "turn:1.2.3.4:3478?transport=tcp", "TCP URI")
+	assert.Contains(t, uris, "turns:127.0.0.1:3479?transport=tcp", "TLS URI")
+
+	key, ok = authHandler(*turnAuth.Username, v1alpha1.DefaultRealm,
+		&net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 1234})
+	assert.True(t, ok, "authHandler ok")
+	assert.Equal(t, key, a12n.GenerateAuthKey(*turnAuth.Username,
+		v1alpha1.DefaultRealm, *turnAuth.Password), "auth handler ok")
+
+	log.Debug("--------------------------------------")
+	log.Debug("editing config file: adding a new listener")
+	l := v1alpha1.ListenerConfig{}
+	basicAuthTestConfig.Listeners[3].DeepCopyInto(&l)
+	watchTestConfig.Listeners = append(watchTestConfig.Listeners, l)
+
+	data, err = json.MarshalIndent(watchTestConfig, "", "  ")
+	assert.NoError(t, err, "JSONify config file")
+
+	assert.NoError(t, f.Truncate(0), "truncate temp file")
+	_, err = f.Seek(0, 0)
+	assert.NoError(t, err, "seek temp file")
+	_, err = f.Write(data)
+	assert.NoError(t, err, "writing JSON config file")
+
+	// let STUNner pick up the config
+	assert.NoError(t, s.Reconcile(watchTestConfig), "reconcile")
+
+	// wait so that the auth-server has comfortable time to pick up the new config
+	time.Sleep(time.Duration(100) * time.Millisecond)
+
+	u = "dummy"
+	param = client.GetTurnAuthParams{Username: &u}
+	turnAuth, err = authClient.GetTurnAuthToken(ctx, &param)
+	assert.NoError(t, err, "TURN auth get")
+
+	assert.NotNil(t, turnAuth, "TURN token nil")
+	assert.NotNil(t, turnAuth.Username, "username nil")
+	// we do not know the username, only that it contains the timestamps plus a colon
+	assert.Regexp(t, regexp.MustCompile(`^\d+:dummy$`), *turnAuth.Username, "username ok")
+	assert.NoError(t, a12n.CheckTimeWindowedUsername(*turnAuth.Username), "username valid")
+	assert.NotNil(t, turnAuth.Password, "password nil")
+	passwd, err = a12n.GetLongTermCredential(*turnAuth.Username, "my-secret")
+	assert.NoError(t, err, "GetLongTermCredential")
+	assert.Equal(t, passwd, *turnAuth.Password, "password ok")
+	assert.NotNil(t, turnAuth.Uris, "URIs nil")
+	uris = *turnAuth.Uris
+	assert.Contains(t, uris, "turn:1.2.3.4:3478?transport=udp", "UDP URI")
+	assert.Contains(t, uris, "turn:1.2.3.4:3478?transport=tcp", "TCP URI")
+	assert.Contains(t, uris, "turns:127.0.0.1:3479?transport=tcp", "TLS URI")
+	assert.Contains(t, uris, "turns:127.0.0.1:3479?transport=udp", "DTLS URI")
+
+	key, ok = authHandler(*turnAuth.Username, v1alpha1.DefaultRealm,
+		&net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 1234})
+	assert.True(t, ok, "authHandler ok")
+	assert.Equal(t, key, a12n.GenerateAuthKey(*turnAuth.Username,
+		v1alpha1.DefaultRealm, *turnAuth.Password), "auth handler ok")
+
+	log.Debug("--------------------------------------")
+	log.Debug("editing config file: adding a new public IP/port to a listener")
+	watchTestConfig.Listeners[3].PublicAddr = "4.3.2.1"
+	watchTestConfig.Listeners[3].PublicPort = 12345
+
+	data, err = json.MarshalIndent(watchTestConfig, "", "  ")
+	assert.NoError(t, err, "JSONify config file")
+
+	assert.NoError(t, f.Truncate(0), "truncate temp file")
+	_, err = f.Seek(0, 0)
+	assert.NoError(t, err, "seek temp file")
+	_, err = f.Write(data)
+	assert.NoError(t, err, "writing JSON config file")
+
+	// let STUNner pick up the config
+	assert.NoError(t, s.Reconcile(watchTestConfig), "reconcile")
+
+	// wait so that the auth-server has comfortable time to pick up the new config
+	time.Sleep(time.Duration(100) * time.Millisecond)
+
+	u = "dummy"
+	param = client.GetTurnAuthParams{Username: &u}
+	turnAuth, err = authClient.GetTurnAuthToken(ctx, &param)
+	assert.NoError(t, err, "TURN auth get")
+
+	assert.NotNil(t, turnAuth, "TURN token nil")
+	assert.NotNil(t, turnAuth.Username, "username nil")
+	// we do not know the username, only that it contains the timestamps plus a colon
+	assert.Regexp(t, regexp.MustCompile(`^\d+:dummy$`), *turnAuth.Username, "username ok")
+	assert.NoError(t, a12n.CheckTimeWindowedUsername(*turnAuth.Username), "username valid")
+	assert.NotNil(t, turnAuth.Password, "password nil")
+	passwd, err = a12n.GetLongTermCredential(*turnAuth.Username, "my-secret")
+	assert.NoError(t, err, "GetLongTermCredential")
+	assert.Equal(t, passwd, *turnAuth.Password, "password ok")
+	assert.NotNil(t, turnAuth.Uris, "URIs nil")
+	uris = *turnAuth.Uris
+	assert.Contains(t, uris, "turn:1.2.3.4:3478?transport=udp", "UDP URI")
+	assert.Contains(t, uris, "turn:1.2.3.4:3478?transport=tcp", "TCP URI")
+	assert.Contains(t, uris, "turns:127.0.0.1:3479?transport=tcp", "TLS URI")
+	assert.Contains(t, uris, "turns:4.3.2.1:12345?transport=udp", "DTLS URI")
+
+	key, ok = authHandler(*turnAuth.Username, v1alpha1.DefaultRealm,
+		&net.UDPAddr{IP: net.ParseIP("4.3.2.1"), Port: 12345})
+	assert.True(t, ok, "authHandler ok")
+	assert.Equal(t, key, a12n.GenerateAuthKey(*turnAuth.Username,
+		v1alpha1.DefaultRealm, *turnAuth.Password), "auth handler ok")
+
+	log.Debug("--------------------------------------")
+	log.Debug("editing config file: changing the proto for a listener")
+	watchTestConfig.Listeners[1].Protocol = "udp"
+
+	data, err = json.MarshalIndent(watchTestConfig, "", "  ")
+	assert.NoError(t, err, "JSONify config file")
+
+	assert.NoError(t, f.Truncate(0), "truncate temp file")
+	_, err = f.Seek(0, 0)
+	assert.NoError(t, err, "seek temp file")
+	_, err = f.Write(data)
+	assert.NoError(t, err, "writing JSON config file")
+
+	// let STUNner pick up the config -- will cause a restart!
+	err = s.Reconcile(watchTestConfig)
+	_, ok = err.(v1alpha1.ErrRestarted)
+	assert.True(t, ok, "reconcile")
+
+	// wait so that the auth-server has comfortable time to pick up the new config
+	time.Sleep(time.Duration(100) * time.Millisecond)
+
+	u = "dummy"
+	param = client.GetTurnAuthParams{Username: &u}
+	turnAuth, err = authClient.GetTurnAuthToken(ctx, &param)
+	assert.NoError(t, err, "TURN auth get")
+
+	assert.NotNil(t, turnAuth, "TURN token nil")
+	assert.NotNil(t, turnAuth.Username, "username nil")
+	// we do not know the username, only that it contains the timestamps plus a colon
+	assert.Regexp(t, regexp.MustCompile(`^\d+:dummy$`), *turnAuth.Username, "username ok")
+	assert.NoError(t, a12n.CheckTimeWindowedUsername(*turnAuth.Username), "username valid")
+	assert.NotNil(t, turnAuth.Password, "password nil")
+	passwd, err = a12n.GetLongTermCredential(*turnAuth.Username, "my-secret")
+	assert.NoError(t, err, "GetLongTermCredential")
+	assert.Equal(t, passwd, *turnAuth.Password, "password ok")
+	assert.NotNil(t, turnAuth.Uris, "URIs nil")
+	uris = *turnAuth.Uris
+	assert.Contains(t, uris, "turn:1.2.3.4:3478?transport=udp", "UDP URI")
+	assert.Contains(t, uris, "turn:1.2.3.4:3478?transport=udp", "TCP URI")
+	assert.Contains(t, uris, "turns:127.0.0.1:3479?transport=tcp", "TLS URI")
+	assert.Contains(t, uris, "turns:4.3.2.1:12345?transport=udp", "DTLS URI")
+
+	key, ok = authHandler(*turnAuth.Username, v1alpha1.DefaultRealm,
+		&net.UDPAddr{IP: net.ParseIP("4.3.2.1"), Port: 12345})
+	assert.True(t, ok, "authHandler ok")
+	assert.Equal(t, key, a12n.GenerateAuthKey(*turnAuth.Username,
+		v1alpha1.DefaultRealm, *turnAuth.Password), "auth handler ok")
 }
